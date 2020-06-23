7f34962c43c9cb530778d41307260225440561cc
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-23 21:42:46 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,21 @@
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = $'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  mario
   puts status
+end
+
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def all_phrases
+  mario
+  toadstool
+  link
 end
